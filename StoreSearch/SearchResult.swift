0dba52c2247b26993755338e4d158686488e9bb6
EduardoVaca <vacalalo@gmail.com>
2016-12-05 16:13:02 +0200

    Add a SearchBar and TableView. The search puts fake data for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Eduardo Vaca on 05/12/16.
+//  Copyright © 2016 Vaca. All rights reserved.
+//
+
+
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
