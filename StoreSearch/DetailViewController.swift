2838c3dd7a858a4db40d333432268217e83902c1
EduardoVaca <vacalalo@gmail.com>
2016-12-13 17:41:21 +0200

    Animate the entrance of the Popup Add a gradient background

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -31,6 +31,7 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
         popupView.layer.cornerRadius = 10
+        view.backgroundColor = UIColor.clear
         
         // Gesture recognizer to close view
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(close))
@@ -101,6 +102,10 @@ extension DetailViewController: UIViewControllerTransitioningDelegate {
     
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BounceAnimationController()
     }
 }
 
